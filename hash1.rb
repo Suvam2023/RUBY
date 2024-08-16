0c97c304ad564b35aeb9936c4de1762e15a90e4a
Suvam Parida <117160759+Suvam2023@users.noreply.github.com>
2024-08-16 12:12:55 +0530

    after creating a hash object you can add items ito it

---
+++ hash1.rb
@@ -0,0 +1,6 @@
+h2 = Hash.new("Some kind of ring")
+h2['treasure1'] = 'Silver ring'
+h2['treasure2'] = 'Gold ring'
+h2['treasure3'] = 'Ruby ring'
+h2['treasure4'] = 'Sapphire ring'
+p(h2['treasure4'])
