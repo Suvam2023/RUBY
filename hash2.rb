d4298579198923d6be75837761814e6d2937ab14
Suvam Parida <117160759+Suvam2023@users.noreply.github.com>
2024-08-16 12:29:08 +0530

    using different hash operations

---
+++ hash2.rb
@@ -0,0 +1,11 @@
+h1 = Hash.new
+h2 = Hash.new
+h1 = {'key1'=>'val1', 'key2'=>'val2', 'key3'=>'val3', 'key4'=>'val4'}
+h2 = {'key1'=>'val1', 'KEY_TWO'=>'val2', 'key3'=>'VALUE_3',
+'key4'=>'val4'}
+p( h1.keys & h2.keys )
+p( h1.values & h2.values )
+p( h1.keys+h2.keys )
+p( h1.values-h2.values )
+p( (h1.keys << h2.keys) )
+p( (h1.keys << h2.keys).flatten.reverse )
