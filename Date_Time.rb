4a2709db6320f2bf11d856b7e77891c073205266
Suvam Parida <117160759+Suvam2023@users.noreply.github.com>
2024-08-17 09:12:47 +0530

    Knowing about the use of "Date" in Ruby

---
+++ Date_Time.rb
@@ -0,0 +1,10 @@
+require 'date'
+mydatevar = Date.parse('17 August 2024')
+puts mydatevar
+puts mydatevar.year
+puts mydatevar.mon
+puts "#{mydatevar.mon}"
+puts mydatevar.mday
+puts mydatevar.wday
+puts mydatevar.yday
+
